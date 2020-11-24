816ed65dd1b963185456aa1bf9bdd2639cc4931b
Ethan <ethan.wright@gmail.com>
2020-11-24 13:19:10 +1100

    removeFromArray completed

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
 const removeFromArray = function() {
+    var newArray = arguments[0];
+    for (i = 1; i < arguments.length; i ++) {
+            if(newArray.indexOf(arguments[i]) >= 0) {
+                var pos = newArray.indexOf(arguments[i]);
+                newArray.splice(pos, 1);
+            }
+        
+    }
+    return newArray;
 
 }
 
