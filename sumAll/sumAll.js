8f4629ae4d27f9c5f48db9258936ba44385e0602
Ethan <ethan.wright@gmail.com>
2020-11-25 10:36:50 +1100

    sumAll completed

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    var total = 0;
+    var runningTotal = 0;
+    var endNum = 0;
+    if (numOne <  0 || numTwo < typeof 0 || isNaN(numOne) || isNaN(numTwo)) {
+        return 'ERROR';
+    }else if (numOne < numTwo) {
+        runningTotal = numOne;  
+        endNum = numTwo
+    } else {
+        runningTotal = numTwo;
+        endNum = numOne; 
+    }
+    
+    for (i = 0; i < endNum; i ++) {
+        total = total + runningTotal;
+        runningTotal++;
+    
+    }
+    return total;
 }
 
 module.exports = sumAll
