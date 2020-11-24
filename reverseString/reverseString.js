49b2866dd5b486456f6ed29c49cf5d856cdb075a
Ethan <ethan.wright@gmail.com>
2020-11-24 11:46:55 +1100

    completed reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    inputReversed = '';
+    for(i = 1; i < input.length +1; i ++){
+        inputReversed += input[input.length - i];
+    }
+    return inputReversed;
 }
 
 module.exports = reverseString
