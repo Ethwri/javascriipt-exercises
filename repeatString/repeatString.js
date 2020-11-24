720deb0761c758cc0de7b9d8bb24c4967be926ea
Ethan <ethan.wright@gmail.com>
2020-11-24 11:22:31 +1100

    completed repeatString. though not without having a peek at the solution

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    var word = '';
+    if (times < 0) {
+        return 'ERROR';
+    }
+    for (i = 0; i < times; i ++) {
+        word += string;
+    }
+    return word;
 }
 
 module.exports = repeatString
