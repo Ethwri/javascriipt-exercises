2923c7f2852063efa50f80596fbce0614eefed12
Ethan <ethan.wright@gmail.com>
2020-11-25 13:29:52 +1100

    tempConversion completed

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,8 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  var fahrenheitConverted = (fahrenheit - 32) * 5/9;
+  return parseFloat(fahrenheitConverted.toFixed(1));
 }
 
-const ctof = function() {
+const ctof = function(celsius) {
+  var celsiusConverted = (celsius * 9/5) + 32;
+  return parseFloat(celsiusConverted.toFixed(1));
 
 }
 
